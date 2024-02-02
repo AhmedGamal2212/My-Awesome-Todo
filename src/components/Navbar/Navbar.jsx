c555c7417677cfc93b27af021a52fe0b65673438
Ahmed Gamal <ahmedgamal.ssaleh@gmail.com>
2024-02-02 21:18:50 +0200

    create navbar component and styles
    - add fontawesome dependencies
    - create navbar and style it using bulma classes
    - add padding to the body element to suit the fixed top navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,14 +1,58 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { BASE_URL } from '../../constants';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleCheck } from '@fortawesome/free-regular-svg-icons';
 
 const Navbar = () => {
 	return (
 		<>
-			<div className='container'>
-				<h1 className='title has-text-centered'>
-					Welcome from the nav
-				</h1>
-				<div className='notification is-primary has-text-centered'>
-					This is a primary notification
+			<div
+				className='navbar is-fixed-top is-black pr-1'
+				role='navigation'
+				aria-label='main navigation'
+			>
+				<div className='navbar-brand'>
+					<Link
+						className='navbar-item'
+						to={`${BASE_URL}`}
+					>
+						<span className='icon-text'>
+							<span className='icon'>
+								<FontAwesomeIcon
+									icon={faCircleCheck}
+                                    size="lg"
+                                    className="pr-1 pt-1"
+								/>
+							</span>
+							<span className='subtitle has-text-white'>
+								My Awesome Todo
+							</span>
+						</span>
+					</Link>
+
+					<a
+						role='button'
+						className='navbar-burger'
+						aria-label='menu'
+						aria-expanded='false'
+					>
+						<span aria-hidden='true'></span>
+						<span aria-hidden='true'></span>
+						<span aria-hidden='true'></span>
+					</a>
+				</div>
+				<div className='navbar-menu'>
+					<div className='navbar-end'>
+						<div className='buttons'>
+							<button className='navbar-item button outlined has-text-weight-bold'>
+								Login
+							</button>
+							<button className='navbar-item button has-text-weight-bold is-success'>
+								Sign up
+							</button>
+						</div>
+					</div>
 				</div>
 			</div>
 		</>
