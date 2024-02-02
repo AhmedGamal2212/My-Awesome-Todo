db576004704e5e4c7d47efe5d93858ece8e9cb20
Ahmed Gamal <ahmedgamal.ssaleh@gmail.com>
2024-02-02 19:10:45 +0200

    basic setup for routing
    - install react router dependencies
    - create the basic routing skeleton in the app including landing page as root, home as tasks, single task page
    - define and export the a constant with the base url of the website

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,28 @@
+import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
+import { BASE_URL } from './constants';
 
 function App() {
 	return (
 		<>
-			<Home />
+			<Routes>
+				<Route path={`${BASE_URL}`}>
+					<Route
+						path=''
+						element={<h1>landing page</h1>}
+					/>
+					<Route path='tasks'>
+						<Route
+							path=''
+							element={<Home />}
+						/>
+						<Route
+							path=':id'
+							element={<h1>single task page</h1>}
+						/>
+					</Route>
+				</Route>
+			</Routes>
 		</>
 	);
 }
