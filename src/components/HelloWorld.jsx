dfe47545497dc6a0fced24b2bb4c4af0f21955cf
Ahmed Gamal <ahmedgamal.ssaleh@gmail.com>
2024-02-02 19:33:44 +0200

    refactor layout to show navbar component instead of the dummy value
    - remove the hello world component
    - refactor home page to use a dummy value instead of hello world component
    - create a navbar component with its basic setup
    - update the layout to use the new navbar component

--- src/components/HelloWorld.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const HelloWorld = ({ location }) => {
-	return <div>Hello World from the {location}</div>;
-};
-
-export default HelloWorld;
--- src/pages/Home.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import HelloWorld from '../components/HelloWorld';
-
-const Home = () => {
-	return <HelloWorld location='Home' />;
-};
-
-export default Home;
