247b4d9238a1c880faf8b4740ca64cdba035c569
Ahmed Gamal <ahmedgamal.ssaleh@gmail.com>
2024-02-03 16:10:06 +0200

    change router to hash router

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import { QueryClient, QueryClientProvider } from 'react-query';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import './index.css';
 import 'bulma/css/bulma.css';
+import { BASE_URL } from './constants.js';
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <Router>
+        <Router basename={`/`}>
             <QueryClientProvider client={queryClient}>
                 <App />
             </QueryClientProvider>
