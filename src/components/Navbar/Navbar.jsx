984b1f6adb23f023f0a5e3b8b2957973089a20b2
Ahmed Gamal <ahmedgamal.ssaleh@gmail.com>
2024-02-03 09:06:00 +0200

    reformat navbar with testing prettier
    - change the useTab setting in pretteir
    - try the formatting in navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,57 +5,59 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCircleCheck } from '@fortawesome/free-regular-svg-icons';
 
 const Navbar = () => {
-	return (
-		<>
-			<div
-				className='navbar is-fixed-top is-black pr-1'
-				role='navigation'
-				aria-label='main navigation'
-			>
-				<div className='navbar-brand'>
-					<Link
-						className='navbar-item'
-						to={`${BASE_URL}`}
-					>
-						<span className='icon-text'>
-							<span className='icon'>
-								<FontAwesomeIcon
-									icon={faCircleCheck}
-									size='lg'
-									className='pr-1 pt-1'
-									shake
-								/>
-							</span>
-							<span className='subtitle has-text-white'>My Awesome Todo</span>
-						</span>
-					</Link>
+    return (
+        <>
+            <div
+                className='navbar is-fixed-top is-black pr-1'
+                role='navigation'
+                aria-label='main navigation'
+            >
+                <div className='navbar-brand'>
+                    <Link
+                        className='navbar-item'
+                        to={`${BASE_URL}`}
+                    >
+                        <span className='icon-text'>
+                            <span className='icon'>
+                                <FontAwesomeIcon
+                                    icon={faCircleCheck}
+                                    size='lg'
+                                    className='pr-1 pt-1'
+                                    shake
+                                />
+                            </span>
+                            <span className='subtitle has-text-white'>
+                                My Awesome Todo
+                            </span>
+                        </span>
+                    </Link>
 
-					<a
-						role='button'
-						className='navbar-burger'
-						aria-label='menu'
-						aria-expanded='false'
-					>
-						<span aria-hidden='true'></span>
-						<span aria-hidden='true'></span>
-						<span aria-hidden='true'></span>
-					</a>
-				</div>
-				<div className='navbar-menu'>
-					<div className='navbar-end'>
-						<div className='buttons'>
-							<button className='navbar-item button outlined has-text-weight-bold'>
-								Login
-							</button>
-							<button className='navbar-item button has-text-weight-bold is-success'>
-								Sign up
-							</button>
-						</div>
-					</div>
-				</div>
-			</div>
-		</>
-	);
+                    <a
+                        role='button'
+                        className='navbar-burger'
+                        aria-label='menu'
+                        aria-expanded='false'
+                    >
+                        <span aria-hidden='true'></span>
+                        <span aria-hidden='true'></span>
+                        <span aria-hidden='true'></span>
+                    </a>
+                </div>
+                <div className='navbar-menu'>
+                    <div className='navbar-end'>
+                        <div className='buttons'>
+                            <button className='navbar-item button outlined has-text-weight-bold'>
+                                Login
+                            </button>
+                            <button className='navbar-item button has-text-weight-bold is-success'>
+                                Sign up
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
 };
 
 export default Navbar;
