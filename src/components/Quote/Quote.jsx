86ba97dc7642928dc5157358654cb0ef22288bc7
Ahmed Gamal <ahmedgamal.ssaleh@gmail.com>
2024-02-03 12:15:32 +0200

    move showing time of the quotes to a constant
    - move the hardcoded showing time of the quotes to constants
    - import the showing time in Quote and use it instead of the hardcoded one
    - remove source key from the quotes

--- src/components/Quote/Quote.jsx
+++ src/components/Quote/Quote.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
 const Quote = ({ quoteDetails }) => {
-    const { quote, author, source } = quoteDetails;
-    return <div>{`${quote} - ${author} - ${source}`}</div>;
+    const { quote, author } = quoteDetails;
+    return <div>{`${quote} - ${author}`}</div>;
 };
 
 export default Quote;
