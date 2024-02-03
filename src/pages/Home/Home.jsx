7f6b9498d29144d6d5bb0518b3fd23be9b836548
Ahmed Gamal <ahmedgamal.ssaleh@gmail.com>
2024-02-03 12:20:07 +0200

    remove dummy data from home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -6,59 +6,6 @@ const Home = () => {
     return (
         <>
             <QuoteContainer useQuote={useRandomInspirationalQuote} />
-            <div style={{ color: 'red' }}>Welcome from Home</div>
-            <div>Welcome from Home</div>
-            <div>Welcome from Home</div>
-            <div>Welcome from Home</div>
-            <div>Welcome from Home</div>
-            <div>Welcome from Home</div>
-            <div>Welcome from Home</div>
-            <div>Welcome from Home</div>
-            <div>Welcome from Home</div>
-            <div>Welcome from Home</div>
-            <div>Welcome from Home</div>
-            <div>Welcome from Home</div>
-            <div>Welcome from Home</div>
-            <div>Welcome from Home</div>
-            <div>Welcome from Home</div>
-            <div>Welcome from Home</div>
-            <div>Welcome from Home</div>
-            <div>Welcome from Home</div>
-            <div>Welcome from Home</div>
-            <div>Welcome from Home</div>
-            <div>Welcome from Home</div>
-            <div>Welcome from Home</div>
-            <div>Welcome from Home</div>
-            <div>Welcome from Home</div>
-            <div>Welcome from Home</div>
-            <div>Welcome from Home</div>
-            <div>Welcome from Home</div>
-            <div>Welcome from Home</div>
-            <div>Welcome from Home</div>
-            <div>Welcome from Home</div>
-            <div>Welcome from Home</div>
-            <div>Welcome from Home</div>
-            <div>Welcome from Home</div>
-            <div>Welcome from Home</div>
-            <div>Welcome from Home</div>
-            <div>Welcome from Home</div>
-            <div>Welcome from Home</div>
-            <div>Welcome from Home</div>
-            <div>Welcome from Home</div>
-            <div>Welcome from Home</div>
-            <div>Welcome from Home</div>
-            <div>Welcome from Home</div>
-            <div>Welcome from Home</div>
-            <div>Welcome from Home</div>
-            <div>Welcome from Home</div>
-            <div>Welcome from Home</div>
-            <div>Welcome from Home</div>
-            <div>Welcome from Home</div>
-            <div>Welcome from Home</div>
-            <div>Welcome from Home</div>
-            <div>Welcome from Home</div>
-            <div>Welcome from Home</div>
-            <div>Welcome from Home</div>
         </>
     );
 };
