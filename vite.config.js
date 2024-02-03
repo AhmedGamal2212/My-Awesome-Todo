6796a78ccee57d15a4b78b0618d96b2db8e5a47a
Ahmed Gamal <ahmedgamal.ssaleh@gmail.com>
2024-02-03 14:23:58 +0200

    update base path in vite config
    - add the ending slash to fix deployment issue

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/my-awesome-todo',
+    base: '/my-awesome-todo/',
     plugins: [react()],
     test: {
         globals: true,
