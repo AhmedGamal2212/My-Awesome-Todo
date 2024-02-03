610cad32f4ea77bbb2c03abf8ab143aa74472ee1
Ahmed Gamal <ahmedgamal.ssaleh@gmail.com>
2024-02-03 21:51:09 +0200

    add tests for Quote component
    - add test ids for the text wrappers to be able to test them properly
    - add unit tests to test the initial value of the quote container
    - add unit tests to test the changed value of the quote container after a specific amount of time

--- src/components/Quote/Quote.jsx
+++ src/components/Quote/Quote.jsx
@@ -6,8 +6,18 @@ const Quote = ({ quoteDetails }) => {
         <div className='container mt-4 p-3 is-fluid'>
             <div class='box p-1'>
                 <div className='has-text-weight-bold notification is-success has-text-centered'>
-                    <span className='is-size-5-desktop is-size-6-mobile is-block'>"{`${quote}`}"</span>
-                    <span className='is-size-6-desktop is-size-7-mobile is-italic'>- {`${author}`}</span>
+                    <span
+                        className='is-size-5-desktop is-size-6-mobile is-block'
+                        data-testid='inspirational-quote-text'
+                    >
+                        "{`${quote}`}"
+                    </span>
+                    <span
+                        className='is-size-6-desktop is-size-7-mobile is-italic'
+                        data-testid='inspirational-quote-author'
+                    >
+                        - {`${author}`}
+                    </span>
                 </div>
             </div>
         </div>
--- src/components/Quote/index.js
+++ src/components/Quote/index.js
@@ -1,2 +1,5 @@
 import QuoteContainer from './QuoteContainer';
+import Quote from './Quote';
+
 export default QuoteContainer;
+export { Quote };
