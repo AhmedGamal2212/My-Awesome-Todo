fd003391ad3f6f0c1fba034a38e64e454d95e0de
Ahmed Gamal <ahmedgamal.ssaleh@gmail.com>
2024-02-03 22:23:57 +0200

    change quote styling
    - change quote styles from being a notification to a regular box

--- src/components/Quote/Quote.jsx
+++ src/components/Quote/Quote.jsx
@@ -3,22 +3,20 @@ import React from 'react';
 const Quote = ({ quoteDetails }) => {
     const { quote, author } = quoteDetails;
     return (
-        <div className='container mt-4 p-3 is-fluid'>
-            <div class='box p-1'>
-                <div className='has-text-weight-bold notification is-success has-text-centered'>
-                    <span
-                        className='is-size-5-desktop is-size-6-mobile is-block'
-                        data-testid='inspirational-quote-text'
-                    >
-                        "{`${quote}`}"
-                    </span>
-                    <span
-                        className='is-size-6-desktop is-size-7-mobile is-italic'
-                        data-testid='inspirational-quote-author'
-                    >
-                        - {`${author}`}
-                    </span>
-                </div>
+        <div className='container mt-4 p-4 is-fluid'>
+            <div className='has-text-white has-text-weight-bold has-text-centered'>
+                <span
+                    className='is-size-5-desktop is-size-6-mobile is-block'
+                    data-testid='inspirational-quote-text'
+                >
+                    "{`${quote}`}"
+                </span>
+                <span
+                    className='is-size-6-desktop is-size-7-mobile is-italic'
+                    data-testid='inspirational-quote-author'
+                >
+                    - {`${author}`}
+                </span>
             </div>
         </div>
     );
