caab1989c0992ea1e9365527b10b275f95dcb7a1
Ahmed Gamal <ahmedgamal.ssaleh@gmail.com>
2024-02-07 12:19:52 +0200

    add new task form control using formik
    - add formik dependencies
    - use formik form control to replace the native form elements
    - add initial values and mock onSubmit

--- src/components/AddTaskForm/AddTaskForm.jsx
+++ src/components/AddTaskForm/AddTaskForm.jsx
@@ -1,13 +1,38 @@
 import React from 'react';
+import { Formik, Field, Form } from 'formik';
+
+const initialValues = {
+    'task-title': '',
+};
+
+const onSubmit = (values, actions) => {
+    console.log(values);
+    actions.resetForm();
+};
 
 const AddTaskForm = () => {
     return (
-        <div className='columns'>
-            <div className='box control has-border-white column is-half is-offset-one-quarter has-background-black is-flex is-justify-content-center is-align-items-center has-text-white is-three-fifths-mobile is-offset-one-fifth-mobile'>
-                <input type="text" className='input' placeholder="Add a new task..." />
-                <button className='button is-success' type='submit'>+</button>
-            </div>
-        </div>
+        <Formik
+            initialValues={initialValues}
+            onSubmit={onSubmit}
+        >
+            <Form className='columns'>
+                <div className='box control has-border-white column is-half is-offset-one-quarter has-background-black is-flex is-justify-content-center is-align-items-center has-text-white is-three-fifths-mobile is-offset-one-fifth-mobile'>
+                    <Field
+                        type='text'
+                        className='input'
+                        placeholder='Add a new task...'
+                        name='task-title'
+                    />
+                    <button
+                        className='button is-success'
+                        type='submit'
+                    >
+                        +
+                    </button>
+                </div>
+            </Form>
+        </Formik>
     );
 };
 
