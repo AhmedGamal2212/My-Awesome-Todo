f705bb9f4eae1735322df92936a9a5204f2c3b3d
Ahmed Gamal <ahmedgamal.ssaleh@gmail.com>
2024-02-07 10:17:11 +0200

    change base url to './'

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/my-awesome-todo/',
+    base: './',
     plugins: [react()],
     test: {
         globals: true,
