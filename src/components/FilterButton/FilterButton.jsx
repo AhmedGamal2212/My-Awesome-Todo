8f1f67b403147b4b450e86892c5d0573bb4ffbcf
Ahmed Gamal <ahmedgamal.ssaleh@gmail.com>
2024-02-07 21:58:59 +0200

    fix button width
    - set min-width fit content to buttons for better responsiveness

--- src/components/FilterButton/FilterButton.jsx
+++ src/components/FilterButton/FilterButton.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import style from './FilterButton.module.css'
 
 const FilterButton = ({ text, action }) => {
     return (
         <button
-            className='button is-success is-fullwidth has-text-weight-bold is-inverted is-outlined has-background-black-bis'
+            className={`button is-success is-fullwidth has-text-weight-bold is-inverted is-outlined has-background-black-bis ${style.btn}`}
             onClick={action}
         >
             {text}
