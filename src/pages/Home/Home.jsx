c04470d4c952c5d1fa370b90c07ae20c578028d2
Ahmed Gamal <ahmedgamal.ssaleh@gmail.com>
2024-02-16 17:30:27 +0200

    remove unnecessary imports in home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,17 +2,16 @@ import React from 'react';
 import { useRandomInspirationalQuote } from '../../hooks/useRandomInspirationalQuote';
 import QuoteContainer from '../../components/Quote/QuoteContainer';
 import AddTaskForm from '../../components/AddTaskForm';
-import SplitScreen from '../../components/SplitScreen';
 import { useTasksContext } from '../../context/TasksContext';
 import RegularItemsListContainer from '../../components/RegularItemsList';
 import TaskFilters from '../../components/TaskFilters';
-import Footer from '../../components/Footer';
 
 const Home = () => {
-    const leftPaneWidthInRatio = 3;
-    const rightPaneWidthInRatio = 1;
     return (
-        <div className='is-fh' style={{position: 'relative'}}>
+        <div
+            className='is-fh'
+            style={{ position: 'relative' }}
+        >
             <QuoteContainer useQuote={useRandomInspirationalQuote} />
             <AddTaskForm />
             <TaskFilters />
