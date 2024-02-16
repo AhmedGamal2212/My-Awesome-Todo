ca9d6731356cf39e5bd519d00ca2188db175a513
Ahmed Gamal <ahmedgamal.ssaleh@gmail.com>
2024-02-16 15:20:36 +0200

    create footer component
    - create footer component with simple credits
    - add footer component to the home page

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,27 @@
+import { faHeart } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer className='p-5 has-background-black'>
+            <div className='content has-text-centered'>
+                <p className='title'>
+                    <span className='has-text-success'>M</span>
+                    ade
+                    <span className='has-text-success'>W</span>ith{' '}
+                    <span className='icon has-text-success'>
+                        <FontAwesomeIcon
+                            icon={faHeart}
+                            beat
+                        />
+                    </span>{' '}
+                    <br />
+                    <span className='subtitle'>- @El_Gemmy</span>
+                </p>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
